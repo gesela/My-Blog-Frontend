c7745fe7ad4ec611a2907de52fa2639d261857ae
Tewodros Yimer <tyimers@gmail.com>
2022-07-02 10:12:52 -0400

    creating folder structure

---
+++ src/components/topbar/TopBar.jsx
@@ -0,0 +1,11 @@
+import "./topbar.css"
+
+export default function TopBar(){
+    return(
+        <div className='top'>
+            <div className="topLeft"></div>
+            <div className="topCenter"></div>
+            <div className="topRight"></div>
+        </div>
+    )
+}
